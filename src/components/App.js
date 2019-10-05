daf7ab8e8e8a9672dc0dccc8716c0b32f09bd680
SongoMen <songomengaming@gmail.com>
2019-10-05 19:37:37 +0200

    file structure

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import { firebaseAuth } from "./auth";
+
+import LandingPage from './landingPage/LandingPage'
+
+function PrivateRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} {...rest} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/Dashboard" />
+        )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  _isMounted = false;
+  state = {
+    authed: false,
+    loading: true,
+    theme: ""
+  };
+  componentDidMount() {
+    this._isMounted = true;
+    this.removeListener = firebaseAuth().onAuthStateChanged(user => {
+      if (this._isMounted) {
+        if (user) {
+          this.setState({
+            authed: true,
+            loading: false
+          });
+        } else {
+          this.setState({
+            authed: false,
+            loading: false
+          });
+        }
+      }
+    });
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+    this.removeListener();
+  }
+
+  render() {
+    return this.state.loading ? (
+      <div className="loader-background">
+        <ul className="loader">
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
+    ) : (
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/landingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="landingPage">
+
+    </div>
+  );
+};
+export default LandingPage;
