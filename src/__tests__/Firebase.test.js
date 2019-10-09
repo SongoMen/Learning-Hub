72b6e1e8c240c8d55c045e4892f61d651c8ae2ba
SongoMen <songomengaming@gmail.com>
2019-10-09 22:37:14 +0200

    firebase tests

---
+++ src/__tests__/Firebase.test.js
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase'
+
+const onAuthStateChanged = jest.fn()
+
+const getRedirectResult = jest.fn(() => {
+  return Promise.resolve({
+    user: {
+      displayName: 'redirectResultTestDisplayName',
+      email: 'redirectTest@test.com',
+    }
+  })
+})
+
+const sendPasswordResetEmail = jest.fn(() => Promise.resolve())
+
+// INITIALIZE APP
+
+jest.spyOn(firebase, 'auth').mockImplementation(() => {
+  return {
+    onAuthStateChanged,
+    currentUser: {
+      displayName: 'testDisplayName',
+      email: 'test@test.com',
+    },
+    getRedirectResult,
+    sendPasswordResetEmail
+  }
+})
+
+test('firebase.auth().getRedirectResult', async () => {
+    const result = await firebase.auth().getRedirectResult()
+    expect(result).toEqual({
+      user: {
+        displayName: 'redirectResultTestDisplayName',
+        email: 'redirectTest@test.com',
+      }
+    })
+    expect(firebase.auth).toHaveBeenCalled()
+    expect(getRedirectResult).toHaveBeenCalled()
+  })
+  
