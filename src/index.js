0cb45d930be7fb2e59aea9c699b3e473dfcd69d1
SongoMen <songomengaming@gmail.com>
2019-11-02 23:51:44 +0100

    b

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./store";
 import { Provider } from "react-redux";
-require('dotenv').config()
 
-console.log(process.env.REACT_APP_VERSION)
 ReactDOM.render(
   <Provider store={configureStore()}>
     <App />
