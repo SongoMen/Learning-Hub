e2980eba9ec246d62bc4d1fd487a7dde9a6b79b2
SongoMen <songomengaming@gmail.com>
2020-01-26 13:47:12 +0100

    s

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,27 +1,29 @@
 import Dashboard from "../components/Dashboard/Dashboard";
 import renderer from "react-test-renderer";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { mount, shallow, configure } from "enzyme";
+import {BrowserRouter as Router} from "react-router-dom";
+import {mount, shallow, configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import configureStore from "../store";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 describe("Test Dashboard page", () => {
-  beforeAll(() => {  
+  beforeAll(() => {
     Object.defineProperty(window, "matchMedia", {
-      value: jest.fn(() => { return { matches: true } })
+      value: jest.fn(() => {
+        return {matches: true};
+      }),
     });
   });
   test("site renders correctly", () => {
     const component = renderer.create(
       <Router>
-      <Provider store={configureStore()}>
-        <Dashboard />
-      </Provider>
-    </Router>
+        <Provider store={configureStore()}>
+          <Dashboard />
+        </Provider>
+      </Router>,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
